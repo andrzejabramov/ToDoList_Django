e5b94139c9f7b0a16bb5d8bdf632442b2f193cee
andrejabramov <andrzejvod@gmail.com>
2024-12-30 22:43:03 +0300

    20241230_todolist_django2

--- todoproject/todo_list/urls.py
+++ todoproject/todo_list/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from .views import TodoItemsListView, TodoItemCreateView, ToDoItemDeleteView, ToDoItemToggleView
 
-
-app_name = "todo_list"
+app_name = "todo-list"
 
 urlpatterns = [
-    # "path"
+    path("", TodoItemsListView.as_view(), name="items-list"),
+    path("create/", TodoItemCreateView.as_view(), name="create-item"),
+    path("<int:pk>/confirm-delete/", ToDoItemDeleteView.as_view(), name="delete-item"),
+    path("<int:pk>/toggle/", ToDoItemToggleView.as_view(), name="toggle-item"),
 ]
--- todoproject/todo_list/views.py
+++ todoproject/todo_list/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView, CreateView, DeleteView, View
 from .models import TodoItem
 
 
-class TodoItemListView(ListView):
+class TodoItemsListView(ListView):
     model = TodoItem
 
+class TodoItemCreateView(CreateView):
+    model = TodoItem
+    fields = [
+        "text",
+    ]
+    success_url = reverse_lazy("todo-list: items-list")
+
+class ToDoItemDeleteView(DeleteView):
+    model = TodoItem
+    success_url = reverse_lazy("todo-list: items-list")
+
+class ToDoItemToggleView(View):
+    def post(self, request: HttpRequest, pk: int) -> HttpResponse:
+        todo_item: TodoItem = get_object_or_404(TodoItem, pk=pk)
+        todo_item.done = not todo_item.done
+        todo_item.save()
+        url = reverse('todo-list:items-list')
+        return redirect(url)
